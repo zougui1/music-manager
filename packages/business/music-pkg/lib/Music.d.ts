975bf3b9126410dd4ef069928df6cd82e5262647
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:17:03 +0100

    Delete packages/business/music-pkg/lib directory

--- packages/business/music-pkg/lib/Music.d.ts
+++
@@ -1,9 +0,0 @@
-import { RepositoryAccessor, MusicEntity, MusicRepository, DeepPartial } from 'database-pkg';
-export declare class Music extends RepositoryAccessor<MusicRepository> {
-    constructor();
-    findMany(): Promise<MusicEntity[]>;
-    findById(id: number): Promise<MusicEntity | undefined>;
-    create(music: DeepPartial<MusicEntity>): Promise<MusicEntity>;
-    clear(): Promise<void>;
-}
-//# sourceMappingURL=Music.d.ts.map
