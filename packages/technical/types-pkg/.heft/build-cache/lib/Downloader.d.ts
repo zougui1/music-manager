edd88a40f9ad16c89ac70748514901260b5efec1
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:27:47 +0100

    Delete packages/technical/types-pkg/.heft/build-cache directory

--- packages/technical/types-pkg/.heft/build-cache/lib/Downloader.d.ts
+++
@@ -1,5 +0,0 @@
-import { DownloadedAudio } from './DownloadedAudio';
-export interface Downloader {
-    downloadAudio(link: string): Promise<DownloadedAudio | DownloadedAudio[]>;
-}
-//# sourceMappingURL=Downloader.d.ts.map
--- packages/technical/types-pkg/.heft/build-cache/lib/object.d.ts
+++
@@ -1,5 +0,0 @@
-export interface ObjectOf<T> {
-    [key: string]: T;
-}
-export declare type ObjectLiteral = ObjectOf<any>;
-//# sourceMappingURL=object.d.ts.map
