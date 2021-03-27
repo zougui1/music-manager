25d3394652273d986ae686ee069649ddf299863a
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:17:16 +0100

    Delete packages/business/notification-pkg/.heft/build-cache directory

--- packages/business/notification-pkg/.heft/build-cache/lib/queue.d.ts
+++
@@ -1,8 +0,0 @@
-import { ManyMessages } from 'mq-pkg';
-import { Observable } from 'rxjs';
-export declare const observeMusicDownloadProgress: (options?: ObserveMusicDownloadProgressOptions | undefined) => Observable<ManyMessages>;
-export declare const notifyMusicDownloadProgress: (data: any) => void;
-export interface ObserveMusicDownloadProgressOptions {
-    bufferTime?: number;
-}
-//# sourceMappingURL=queue.d.ts.map
