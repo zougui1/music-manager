2502e90ac56982916c6b14de979e3fe6a1cbe3e0
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:26:48 +0100

    Delete packages/technical/hub-publish-pkg/lib directory

--- packages/technical/hub-publish-pkg/lib/env.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const HUB_URL: string;
-export declare const PUBLISHER_JWT: string;
-//# sourceMappingURL=env.d.ts.map
--- packages/technical/hub-publish-pkg/lib/getPublishToken.d.ts
+++
@@ -1,3 +0,0 @@
-import jwt from 'jsonwebtoken';
-export declare const getPublishToken: (topic: string, key: jwt.Secret, options?: jwt.SignOptions) => string;
-//# sourceMappingURL=getPublishToken.d.ts.map
