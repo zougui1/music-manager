b36dc2e67cdc1f975950844cc1dc94b606a3b71c
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:20:10 +0100

    Delete packages/business/youtube-pkg/lib directory

--- packages/business/youtube-pkg/lib/errors.d.ts
+++
@@ -1,8 +0,0 @@
-declare const YoutubeInvalidUrlError_base: import("error-pkg").ApiError<import("types-pkg").ObjectLiteral>;
-export declare class YoutubeInvalidUrlError extends YoutubeInvalidUrlError_base {
-}
-declare const YoutubeVideoNotFoundError_base: import("error-pkg").ApiError<import("types-pkg").ObjectLiteral>;
-export declare class YoutubeVideoNotFoundError extends YoutubeVideoNotFoundError_base {
-}
-export {};
-//# sourceMappingURL=errors.d.ts.map
--- packages/business/youtube-pkg/lib/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const getYoutubeUrl: (url: string) => string;
-export declare const getThumbnailUrl: (videoId: string) => string;
-export declare const getYoutubeId: (urlOrId: string) => string | undefined;
-//# sourceMappingURL=utils.d.ts.map
