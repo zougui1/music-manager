c647ea76acdd1706dbadb0482216690d1ef76387
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:20:01 +0100

    Delete packages/business/youtube-pkg/.heft/build-cache directory

--- packages/business/youtube-pkg/.heft/build-cache/lib/getMostCorrectVideo.d.ts
+++
@@ -1,4 +0,0 @@
-import { YoutubeVideo } from './YoutubeVideo';
-import { TestedYoutubeVideo, ExpectedData } from './types';
-export declare const getMostCorrectVideo: (videos: YoutubeVideo[], expectedData: ExpectedData) => Promise<TestedYoutubeVideo>;
-//# sourceMappingURL=getMostCorrectVideo.d.ts.map
