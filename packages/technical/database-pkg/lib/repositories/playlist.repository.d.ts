bb22ccb34917e9afed8711a44c0326fbff723ee5
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:20:52 +0100

    Delete packages/technical/database-pkg/lib directory

--- packages/technical/database-pkg/lib/repositories/playlist.repository.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseRepository } from './base.repository';
-import { PlaylistEntity } from '../entities';
-export declare class PlaylistRepository extends BaseRepository<PlaylistEntity> {
-}
-//# sourceMappingURL=playlist.repository.d.ts.map
