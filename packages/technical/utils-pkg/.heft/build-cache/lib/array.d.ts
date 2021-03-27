24f31a40059fa5c7f36ece66560110a9c7ca4d8b
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:28:14 +0100

    Delete packages/technical/utils-pkg/.heft/build-cache directory

--- packages/technical/utils-pkg/.heft/build-cache/lib/array.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const toArray: <T>(value: T | T[]) => T[];
-export declare const last: <T>(array: T[]) => T | undefined;
-//# sourceMappingURL=array.d.ts.map
--- packages/technical/utils-pkg/.heft/build-cache/lib/date.d.ts
+++
@@ -1,3 +0,0 @@
-import moment from 'moment';
-export declare const formatDuration: (duration: number | string | Date | moment.Moment) => string;
-//# sourceMappingURL=date.d.ts.map
