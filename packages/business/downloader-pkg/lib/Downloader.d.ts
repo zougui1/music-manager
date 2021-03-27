33f3150155f1e3281b235a9e11289528b4b4d48c
zougui <35573266+zougui1@users.noreply.github.com>
2021-03-27 19:16:40 +0100

    Delete packages/business/downloader-pkg/lib directory

--- packages/business/downloader-pkg/lib/Downloader.d.ts
+++
@@ -1,8 +0,0 @@
-import { Downloader as IDownloader, DownloadedAudio } from 'types-pkg';
-export declare class Downloader implements IDownloader {
-    readonly urlOrId: string;
-    readonly downloader: IDownloader;
-    constructor(urlOrId: string);
-    downloadAudio(): Promise<DownloadedAudio[]>;
-}
-//# sourceMappingURL=Downloader.d.ts.map
